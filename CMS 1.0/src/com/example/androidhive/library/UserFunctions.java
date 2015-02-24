86811d3f466c474f1da9ee2b6aa4caefdcf372da
srinivasn-squaresoft <srinivasan.squaresoft@gmail.com>
2015-02-24 19:03:48 +0530

    24-02-last

--- CMS 1.0/src/com/example/androidhive/library/UserFunctions.java
+++ CMS 1.0/src/com/example/androidhive/library/UserFunctions.java
@@ -11,8 +11,10 @@ import java.util.List;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Activity;
 import android.content.Context;
 
 public class UserFunctions {
@@ -20,11 +22,14 @@ public class UserFunctions {
 	//private static Ipaddress ip;
 	private JSONParser jsonParser;
 	
-	private static String loginURL = "http://192.168.56.1/cms/index.php";
-	private static String registerURL = "http://192.168.56.1/cms/index.php";
+	private static String URL = "http://192.168.56.1/cms/index.php?XDEBUG_SESSION_START=netbeans-xdebug";
 	private static String login_tag = "login";
 	private static String register_tag = "register";
-	
+	private static String timetable_tag = "timetable";
+	private static String KEY_SUCCESS = "success";
+	private static String KEY_ERROR = "error";
+	private static String KEY_ERROR_MSG = "error_msg";
+	private static String KEY_DAY = "day";
 	// constructor
 	public UserFunctions(){
 		jsonParser = new JSONParser();
@@ -41,11 +46,20 @@ public class UserFunctions {
 		params.add(new BasicNameValuePair("tag", login_tag));
 		params.add(new BasicNameValuePair("email", email));
 		params.add(new BasicNameValuePair("password", password));
-		JSONObject json = jsonParser.getJSONFromUrl(loginURL, params);
+		JSONObject json = jsonParser.getJSONFromUrl(URL, params);
 		// return json
 		// Log.e("JSON", json.toString());
 		return json;
 	}
+	public JSONObject Usertimetable(String class_id) {
+		// TODO Auto-generated method stub
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("tag", timetable_tag));
+		params.add(new BasicNameValuePair("class_id", class_id));
+		JSONObject json = jsonParser.getJSONFromUrl(URL, params);
+		return json;
+	}
+	
 	
 	/**
 	 * function make Login Request
@@ -74,7 +88,27 @@ public class UserFunctions {
 		params.add(new BasicNameValuePair("password", password));
 		
 		// getting JSON Object
-		JSONObject json = jsonParser.getJSONFromUrl(registerURL, params);
+		JSONObject json = jsonParser.getJSONFromUrl(URL, params);
+		// return json
+		return json;
+	}
+	
+	public JSONObject registerUser2(String name){
+		// Building Parameters
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("tag", timetable_tag));
+		params.add(new BasicNameValuePair("name", name));
+		params.add(new BasicNameValuePair("lname", name));
+		params.add(new BasicNameValuePair("address", name));
+		params.add(new BasicNameValuePair("role", name));
+		params.add(new BasicNameValuePair("bloodgroup", name));
+		params.add(new BasicNameValuePair("department", name));
+		params.add(new BasicNameValuePair("mobile", name));
+		params.add(new BasicNameValuePair("email", name));
+		params.add(new BasicNameValuePair("password", name));
+		
+		// getting JSON Object
+		JSONObject json = jsonParser.getJSONFromUrl(URL, params);
 		// return json
 		return json;
 	}
@@ -108,5 +142,10 @@ public class UserFunctions {
 		db.resetTables();
 		return true;
 	}
+
+
+
 	
+
+
 }
--- CMS 1.0/src/com/example/cms/TimetableActivity.java
+++ CMS 1.0/src/com/example/cms/TimetableActivity.java
@@ -1,5 +1,11 @@
 package com.example.cms;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.example.androidhive.library.UserFunctions;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -21,6 +27,21 @@ public class TimetableActivity extends Fragment {
 	TextView tv,tv2;
 	TableRow row,row2;
 	TableLayout table;
+	
+	private static String KEY_SUCCESS = "success";
+	private static String KEY_ERROR = "error";
+	private static String KEY_ERROR_MSG = "error_msg";
+	private static String KEY_DAY = "day";
+	private static String KEY_PERIOD1 = "period1";
+	private static String KEY_PERIOD2 = "period2";
+	private static String KEY_PERIOD3 = "period3";
+	private static String KEY_PERIOD4 = "period4";
+	private static String KEY_PERIOD5 = "period5";
+	private static String KEY_PERIOD6 = "period6";
+	private static String KEY_PERIOD7 = "period7";
+	private static String KEY_PERIOD8 = "period8";
+	 UserFunctions userFunction = new UserFunctions();
+	
 	public TimetableActivity(){}
 	
 	@Override
@@ -29,20 +50,46 @@ public class TimetableActivity extends Fragment {
  
         View rootView = inflater.inflate(R.layout.fragment_timetable, container, false);
         
-   		
-   		
+
        Button  btnLogout= (Button) rootView.findViewById(R.id.timbutton);
        btnLogout.setOnClickListener(new OnClickListener() {
-		
+    	  
+    	   
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			Toast.makeText(getActivity(), "Done", 3000).show();
+		   	   String class_id="1";
+		   	   
+		       
+		   	UserFunctions userFunction = new UserFunctions();
+			JSONObject json = userFunction.registerUser2(class_id);
 			
-       		
+//		     try {
+//				String str=json.getString(KEY_SUCCESS);
+//				str=json.getString(KEY_DAY);
+//				str=json.getString(KEY_PERIOD1);
+//				str=json.getString(KEY_PERIOD2);
+//				str=json.getString(KEY_PERIOD3);
+//				str=json.getString(KEY_PERIOD4);
+//				str=json.getString(KEY_PERIOD5);
+//				str=json.getString(KEY_PERIOD6);
+//				str=json.getString(KEY_PERIOD7);
+//				str=json.getString(KEY_PERIOD8);
+//			} catch (JSONException e) {
+//				
+//				e.printStackTrace();
+//			}
+//		        
 		}
 	});
+       
+       
+       HashMap<String,String> user = userFunction.getUserDetails(getActivity());
+       
+       
        for(int i=0;i<7;i++){ 
+    	   
        table = (TableLayout)rootView.findViewById(R.id.the_table);
   		row = new TableRow(this.getActivity());
   		row2 = new TableRow(this.getActivity());
